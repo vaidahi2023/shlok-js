95383b4fa863c609e5c4c40be699168a09344d3a
vaidahi2023 <vaidahi810@gmail.com>
2024-11-26 12:55:06 +0530

    basic file

---
+++ b1.js
@@ -0,0 +1,40 @@
+// word and keyword
+/*
+word--in,out,shlok,boy
+keyword--for,while,if,break,console,log
+c
+dataype--int,float,string
+js 
+var,let,const
+var have globe scope
+let have local scope
+*/
+// var a=10
+// console.log("a="+a)
+// console.log("a=",a)
+// var b="ram"
+// console.log("b="+b)
+// console.log(typeof a)
+// console.log(typeof b)
+// let c=10
+// console.log("c="+c)
+// let d="ram"
+// console.log("d="+d)
+// console.log(typeof a)
+// console.log(typeof b)
+// console.log(typeof c)
+// console.log(typeof d)
+// {
+//     let f=20
+//     var g=25
+//     console.log(f)
+//     console.log(g)
+// }
+// console.log(f)
+// console.log(a)
+
+const a=10
+a=20
+console.log(a)
+
+//console.log("external js file")
