319fd49b664635207f1a8b75a0db89c87ffa6902
vaidahi2023 <vaidahi810@gmail.com>
2025-01-27 15:56:48 +0530

    foreach loop

---
+++ math.js
@@ -0,0 +1,19 @@
+var a=23.5
+
+// console.log(Math.round(4.2))
+// console.log(Math.round(4.6))
+// console.log(Math.ceil(4.6))
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(4.2))
+// console.log(Math.floor(4.6))
+// console.log(Math.trunc(4.6))
+// console.log(Math.trunc(4.2))
+// console.log(Math.PI)
+// console.log(Math.E)
+// console.log(Math.LOG10E)
+// console.log(Math.SQRT2)
+// console.log(Math.pow(2,3))
+// console.log(Math.min(1,3,4,5))
+// console.log(Math.max(1,2,4,5,6))
+
+console.log(Math.abs(-2.4))
